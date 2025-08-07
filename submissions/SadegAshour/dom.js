f963c33031db20dfb787f0b9cfde83aeda407da7
sadeg ashour <sadeg.magde@outlook.sa>
2025-08-07 20:42:27 +0300

    update dom js

--- submissions/SadegAshour/dom.js
+++ submissions/SadegAshour/dom.js
@@ -1,8 +1,8 @@
 console.log("here");
 console.log(document.body);
-console.log(document.body.lastElementChild.tagName);// here log the script tag
+console.log(document.body.lastElementChild.tagName); // here log the script tag
 
-console.log(`number of element children ${document.body.children.length}`);//
+console.log(`number of element children ${document.body.children.length}`); //
 
 const differ = document.body.childNodes.length - document.body.children.length;
 console.log(`difference in length between childNodes and children ${differ}`);
@@ -11,9 +11,9 @@ console.log(`difference in length between childNodes and children ${differ}`);
 //What is the nodeType and nodeName of the first node in document.body.childNodes?
 var firstNode = document.body.childNodes[0];
 
-console.log(`first node name: ${firstNode.nodeName}`);// here log just the name of node like text
-console.log(`first node type: ${firstNode.nodeType}`);// here log the code of the type that if we see in Node object that we have many type ELEMENT_NODE: 1; ATTRIBUTE_NODE: 2; TEXT_NODE: 3; and so on 
-   
+console.log(`first node name: ${firstNode.nodeName}`); // here log just the name of node like text
+console.log(`first node type: ${firstNode.nodeType}`); // here log the code of the type that if we see in Node object that we have many type ELEMENT_NODE: 1; ATTRIBUTE_NODE: 2; TEXT_NODE: 3; and so on
+
 //Is the first paragraph a sibling of the second, or a descendant?
 // sibling
 
@@ -21,15 +21,25 @@ console.log(document.body.childNodes);
 
 //Twist: Can you find any unexpected text nodes in the DOM structure? Log them and explain their origin
 // i find the text node after my element that their origin to new line \n as we learn
+
+document.body.childNodes.forEach(function (node) {
+  if (node.nodeType == 3) {
+    console.log(`here unexpected node element ${node.nodeName}`);
+  }
+});
+
 /**--------------------------------------------------------*/
 
 var element = document.createElement("div");
 element.className = "card";
 element.setAttribute("data-role", "admin");
-element.innerHTML = `
-  <h2>Access Panel</h2>
-  <p>Authenticated</p>
-`;
+var h2 = document.createElement("h2");
+h2.textContent = "Access Panel";
+
+var p = document.createElement("p");
+p.textContent = "Authenticated";
+
+element.append(h2, p);
 document.body.appendChild(element);
 //Log the value of the data-role as a JS property, not via `.getAttribute`.
 console.log(element.dataset["role"]);
@@ -40,6 +50,10 @@ paragraph[1].textContent = "Welcome back, Admin;";
 //Add two classes to the div: "authenticated" and "highlight" using `classList`
 element.classList.add("authenticated");
 element.classList.add("highlight");
+
+console.log(element.classList.contains("card")); //to verify that "card" still exists
+element.classList.remove("card");
+console.log(element.classList.contains("card")); //to verify that "card" removed
 
 // Extra Section -------------
 const greetTeacher = document.createElement("div");
